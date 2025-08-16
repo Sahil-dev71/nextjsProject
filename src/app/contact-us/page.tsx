a8e67dc84584f5143367596a5e2bff7e7d25d66f
sahilgupta-88 <sahilgupta9856@gmail.com>
2025-08-16 12:50:50 +0530

    fixing responsiveness of contact-us page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -7,18 +7,23 @@ function page() {
     const[name,setName]=useState("");
     const[phone,setPhone]=useState("");
     const handleEvent=()=>{
+      const regex=/^(?:\+?91[-\s]?|0)?[6-9]\d{9}$/;
         if(name==""){
             alert("Please fill the form ");
             return;
+        }else if(!phone.match(regex)){
+          alert("you should have to enter a valid phone number");
+          setPhone("");
+          return;
         }
         alert(`hey!${name},your response is Submitted`)
     }
   return (
-    <div className='flex flex-col justify-center items-center bg-[url(../../public/musicSchool.jpeg)] h-screen bg-no-repeat bg-cover backdrop-brightness-50 bg-bottom'>
+    <div className='flex flex-col justify-center items-center bg-[url(../../public/musicSchool.jpeg)]  h-screen bg-no-repeat bg-cover backdrop-brightness-50 bg-bottom w-full mx-auto'>
          <div className="absolute inset-0 bg-black/30"></div>
         <div className='text-center flex flex-col justify-center items-center bg-gray-600 w-[35rem] rounded-3xl h-[50em] my-20 z-10 '>
           <div className='w-[26rem] text-left'>
-            <h2 className='font-bold text-2xl text-red-300 '> Note :</h2>
+            <h2 className='font-bold text-2xl text-red-300 '>Note :</h2>
              <p className=''>Have questions about our classes, schedules, or programs? Fill out the form below and our team will get back to you with all the details you need to start your musical journey</p>
            </div>
         <div className='border-[2px] border-amber-300 rounded-3xl w-[25rem] h-15 text-center mt-4'>
@@ -26,12 +31,12 @@ function page() {
         </div>
      <div className='my-4 flex flex-row justify-center items-center'> {/* Input fields of the form */}
          <div className='my-4 text-left flex flex-col'>
-        <label htmlFor="name" className='mx-2 my-4 text-[1.25rem]' > Name : </label>
+        <label htmlFor="name" className='mx-2 my-4 text-[1.25rem]' >Name : </label>
         <label htmlFor="phone" className='mx-2 my-4 text-[1.25rem]'>Phone Number : </label>
       </div>
       <div className='flex flex-col' >
         <input type="text" id='name' placeholder='Enter your name here....' onChange={e=>setName(e.target.value)} className='focus:outline-1 focus:outline-amber-300 rounded-2xl h-8 px-2 border-[0.25px] my-4'/>
-       <input type="tel" pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' id='phone' placeholder='Enter your contact number' onChange={e=>setPhone(e.target.value)}  className='focus:outline-1 focus:outline-amber-300 rounded-2xl h-8 px-2 border-[0.25px] my-4' />
+       <input type="tel" id='phone' placeholder='Your contact number....' onChange={e=>setPhone(e.target.value)}  className='focus:outline-1 focus:outline-amber-300 rounded-2xl h-8 px-2 border-[0.25px] my-4' />
       </div>
      </div>
      <button type='submit' onClick={handleEvent} className='my-4 border-2 rounded-2xl w-[100px] h-[35px] hover:bg-blue-400'>Submit</button>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import { px } from "motion";
+import React from "react";
 
 function Footer() {
   return (
-    <div>
-    <footer>
-        <div className='bg-gray-700 text-white grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 h-[15rem] text-center py-6'>
-            <div className='w-[11rem] mx-8 text-center flex flex-col justify-center items-center'>
-              <h4 className='font-bold'>About-us</h4>
-              <p className=' max-auto text-center mt-2 '>
-                Inspiring a love for music through lessons, passion, and creativity for all ages</p>
-            </div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+    <div className="mb-0">
+      <footer>
+        <div className="bg-gray-700 text-white grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 text-center py-6">
+          <div>
+            <img
+              src="/file.svg"
+              alt="Music School"
+              className="h-32 mix-blend-multiply  bg-gray-700 text-white "
+            />
+          </div>
+          <div className="w-auto mx-8 text-center flex flex-col justify-center items-center">
+            <h3 className="font-bold">About-us</h3>
+            <p className=" max-auto text-center mt-2 font-light ">
+              Inspiring a love for music through lessons, passion, and
+              creativity for all ages
+            </p>
+          </div>
+          <div className="w-auto mx-8 text-center flex flex-col justify-center items-center">
+            <h3 className="font-bold"> Contact Us</h3>
+            <p className="max-auto text-center mt-2 font-light">
+              XYZ School of Music,42 Evergreen Heights <br /> Sector 15,
+              Greenview Colony <br />
+              Pune,Maharastra 411001, India
+            </p>
+          </div>
+          <div>4</div>
         </div>
-    </footer>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
